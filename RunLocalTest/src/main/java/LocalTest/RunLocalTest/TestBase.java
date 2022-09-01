ef7ad9ab1e5e0c64d4c5d68b01bf6a58144812f0
Admin <Admin@DESKTOP-R0P7LMN>
2022-09-01 18:07:35 +0530

    push4

--- RunLocalTest/src/main/java/LocalTest/RunLocalTest/TestBase.java
+++ RunLocalTest/src/main/java/LocalTest/RunLocalTest/TestBase.java
@@ -6,6 +6,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
+import org.testng.annotations.Test;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 
 import java.io.File;
 import java.io.IOException;
